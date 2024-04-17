8efdcbf63eec2038ca64f7fb9b5c4e37c496e6cc
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-17 18:49:58 +0800

    dump

---
+++ scripts/dump.sql
@@ -0,0 +1,21 @@
+-- sqlite3 dump.sql
+BEGIN TRANSACTION;
+
+-- CreateTable
+CREATE TABLE "Templates" (
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL UNIQUE,
+    "template" BINARY NOT NULL,
+    "updatedAt" DATETIME NOT NULL
+);
+
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Templates_id_key" ON "Templates"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Templates_name_key" ON "Templates"("name");
+
+-- end of dump
+COMMIT;
