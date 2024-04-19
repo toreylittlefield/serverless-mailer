9712f91a57efa5e1c188ee35ecf8ef89f928f0d4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-19 18:36:37 +0800

    add turso env

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -13,4 +13,6 @@ export const ENV_VARS = {
   NODE_MAILER_FROM: process.env['NODE_MAILER_FROM'] as string,
   NODE_MAILER_SUBJECT: process.env['NODE_MAILER_SUBJECT'] as string,
   NODE_MAILER_SECRET: process.env['NODE_MAILER_SECRET'] as string,
+  TURSO_DATABASE_URL: process.env['TURSO_DATABASE_URL'] as string,
+  TURSO_AUTH_TOKEN: process.env['TURSO_AUTH_TOKEN'] as string,
 } as const;
