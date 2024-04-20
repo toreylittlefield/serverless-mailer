a754eef0fd1a0d6529ac30d17975ed4cc90b83aa
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-21 00:39:41 +0800

    include tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     // ...
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    // TODO: add test database connection
+    // env: {
+    //   NODE_ENV: 'test',
+    // },
   },
 });
