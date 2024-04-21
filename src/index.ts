45050832d3cf7d97339664a0b18c8180e99685ce
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-21 23:37:25 +0800

    export the db CRUD and email send from src/index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import { ENV_VARS } from './helpers/constants.js';
 import { validateEnvVars } from './helpers/utils.js';
-
+import { sendEmail } from './services/nodeMailer.js';
+import {
+  getTemplateByName,
+  getTemplateById,
+  createTemplate,
+  putTemplateByName,
+  deleteAllTemplates,
+  deleteTemplateById,
+  deleteTemplateByName,
+  putTemplateById,
+} from './services/dbClient.js';
 // Validate environment variables before running the application
 validateEnvVars(ENV_VARS);
+
+export {
+  sendEmail,
+  getTemplateByName,
+  getTemplateById,
+  createTemplate,
+  putTemplateByName,
+  deleteAllTemplates,
+  deleteTemplateById,
+  deleteTemplateByName,
+  putTemplateById,
+};
