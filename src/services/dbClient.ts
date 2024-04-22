a861f2481b1770a7a4c7c82eaf699187af36013f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-22 18:18:42 +0800

    map rows to columns and convert bigint to number

--- src/services/dbClient.ts
+++ src/services/dbClient.ts
@@ -1,5 +1,7 @@
+import type { ResultSet } from '@libsql/client/web';
 import { createClient } from '@libsql/client/web';
 import { ENV_VARS } from '../helpers/constants.js';
+import type { Results, Template } from '../helpers/types.js';
 
 // console.log(`Running in ${ENV_VARS.NODE_ENV} environment`);
 console.dir(ENV_VARS);
@@ -10,6 +12,31 @@ const dbClient = createClient({
   authToken: ENV_VARS.TURSO_AUTH_TOKEN,
 });
 
+const mapRowsToColumns = (result: ResultSet): Template[] => {
+  const rows = result.rows.map((row) => {
+    // row to column mapping
+    const columns = Array.from(row).reduce((acc, value, index) => {
+      const columnName = result.columns[index] as keyof Template;
+      if (columnName) {
+        // @ts-expect-error - mapping to Template
+        acc[columnName] = value as Template[keyof Template];
+      }
+      return acc;
+    }, {} as Template);
+
+    return columns;
+  });
+
+  return rows;
+};
+
+const bigIntToNumber = (value: bigint | undefined): number | undefined => {
+  if (!value) {
+    return undefined;
+  }
+  return Number(value);
+};
+
 /**
  * Crud operations on dbClient
  */
@@ -17,97 +44,129 @@ const dbClient = createClient({
 /**
  * GET template by name
  */
-const getTemplateByName = async (name: string) => {
+const getTemplateByName = async (name: string): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'SELECT * FROM Templates WHERE name = ?',
     args: [name],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * GET template by id
  */
-const getTemplateById = async (id: number) => {
+const getTemplateById = async (id: number): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'SELECT * FROM Templates WHERE id = ?',
     args: [id],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * POST template by name
  */
-const createTemplate = async (name: string, template: string) => {
+const createTemplate = async (name: string, template: string): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'INSERT INTO Templates (name, template) VALUES (?, ?)',
     args: [name, template],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * PUT template by name
  */
-const putTemplateByName = async (name: string, template: string) => {
+const putTemplateByName = async (name: string, template: string): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'UPDATE Templates SET template = ? WHERE name = ?',
     args: [template, name],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * PUT template by id
  */
-const putTemplateById = async (id: number, template: string) => {
+const putTemplateById = async (id: number, template: string): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'UPDATE Templates SET template = ? WHERE id = ?',
     args: [template, id],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * DELETE template by name
  */
-const deleteTemplateByName = async (name: string) => {
+const deleteTemplateByName = async (name: string): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'DELETE FROM Templates WHERE name = ?',
     args: [name],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * DELETE template by id
  */
-const deleteTemplateById = async (id: number) => {
+const deleteTemplateById = async (id: number): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'DELETE FROM Templates WHERE id = ?',
     args: [id],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 /**
  * DELETE all templates
  */
-const deleteAllTemplates = async () => {
+const deleteAllTemplates = async (): Promise<Results> => {
   const result = await dbClient.execute({
     sql: 'DELETE FROM Templates',
     args: [],
   });
 
-  return result;
+  return {
+    ...result,
+    lastInsertRowid: bigIntToNumber(result.lastInsertRowid),
+    rows: mapRowsToColumns(result),
+  };
 };
 
 export {
