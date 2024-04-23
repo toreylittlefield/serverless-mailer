9f2bd3abcbb12d69a340595e04a206a288830cad
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-23 14:10:17 +0800

    refactor mailer to accept options as params

--- src/services/nodeMailer.ts
+++ src/services/nodeMailer.ts
@@ -1,8 +1,14 @@
 import nodemailer from 'nodemailer';
+import type { SendMailOptions } from 'nodemailer';
 import { ENV_VARS } from '../helpers/constants.js';
+import { RemoveOptional } from '../helpers/types.js';
 
-const replaceSecret = (html: string, secret: string) => {
-  return html.replace('{{ SECRET_PLACEHOLDER }}', secret);
+type HTML = RemoveOptional<Pick<SendMailOptions, 'html'>>;
+type HTMLString = NonNullable<HTML['html']>;
+
+const replaceSecret = (html: HTMLString, secret: string) => {
+  // if buffer or stream convert to string
+  return html.toString().replace('{{ SECRET_PLACEHOLDER }}', secret);
 };
 
 const transporter = nodemailer.createTransport({
@@ -17,17 +23,21 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// async..await is not allowed in global scope, must use a wrapper
-export const sendEmail = async (htmlTemplate: string) => {
-  // send mail with defined transport object
+export type SendEmailParams = SendMailOptions & {
+  html: HTMLString;
+};
+
+export const sendEmail = async ({ html, ...options }: SendEmailParams) => {
   const info = await transporter.sendMail({
     from: ENV_VARS.NODE_MAILER_FROM, // sender address
     to: ENV_VARS.NODE_MAILER_TO,
     subject: ENV_VARS.NODE_MAILER_SUBJECT,
     text: `Secret: ${ENV_VARS.NODE_MAILER_SECRET}`, // plain text body
-    html: replaceSecret(htmlTemplate, ENV_VARS.NODE_MAILER_SECRET), // html body
+    html: replaceSecret(html, ENV_VARS.NODE_MAILER_SECRET), // html body
+    ...options,
   });
 
   console.log('Message sent: %s', info.messageId);
   // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  return info;
 };
