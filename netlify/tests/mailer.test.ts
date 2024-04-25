c661c77662e6146312fcb8820bb3f1ad18b1d516
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2024-04-25 17:13:33 +0800

    add unit tests for validate body

--- netlify/tests/mailer.test.ts
+++ netlify/tests/mailer.test.ts
@@ -3,8 +3,9 @@ dotenv.config();
 
 import { afterAll, beforeAll, beforeEach, describe, expect, test } from 'vitest';
 import type { SendEmailParams } from '../../src/index.js';
-import type { SuccessMailerResponse } from '../functions/mailer.js';
+import { validateBody, type SuccessMailerResponse } from '../functions/mailer.js';
 import { sendEmailRequest } from './test-utils.js';
+import { ENV_VARS } from '../../src/helpers/constants.js';
 
 const TEST_TEMPLATE_INPUT = {
   subject: 'Test Template',
@@ -36,6 +37,69 @@ afterAll(async () => {});
 beforeEach(async () => {});
 
 const DEFAULT_TIMEOUT = 10000 as const satisfies number;
+
+describe('validateBody', async () => {
+  test('it should return undefined if the body is missing', async () => {
+    expect(validateBody('')).toBeUndefined();
+  });
+
+  test('it should return undefined if the body is not valid JSON', async () => {
+    expect(validateBody('test')).toBeUndefined();
+  });
+
+  test('it should return undefined if the body is missing the "html" field', async () => {
+    expect(validateBody(JSON.stringify({ subject: 'Test Template' }))).toBeUndefined();
+  });
+
+  test('it should return the body if the body is valid JSON and has the "html" field', async () => {
+    expect(validateBody(JSON.stringify(TEST_TEMPLATE_RECORD))).toStrictEqual(TEST_TEMPLATE_RECORD);
+  });
+
+  test('it should filter the "to", "cc", and "bcc" fields for unintended recipients', async () => {
+    const body = JSON.stringify({
+      ...TEST_TEMPLATE_RECORD,
+      to: ['foo', 'foo@bar', 'foo@bar.email.test'],
+      cc: ['cc@foo', 'cc@bar', 'cc@bar.email.test'],
+      bcc: ['bcc@foo', 'bcc@bar', 'bcc@bar.email.test'],
+    });
+    const res = validateBody(body);
+    expect(res).toStrictEqual(TEST_TEMPLATE_RECORD);
+  });
+
+  test('it should set the "to" field if a allowed recipient is provided', async () => {
+    const body = JSON.stringify({ ...TEST_TEMPLATE_RECORD, to: [ENV_VARS.NODE_MAILER_FROM] });
+    const res = validateBody(body);
+    expect(res).toStrictEqual({ ...TEST_TEMPLATE_RECORD, to: [ENV_VARS.NODE_MAILER_FROM] });
+  });
+
+  test('it should set the "cc" field if a allowed recipient is provided', async () => {
+    const body = JSON.stringify({
+      ...TEST_TEMPLATE_RECORD,
+      cc: [ENV_VARS.NODE_MAILER_FROM, ENV_VARS.NODE_MAILER_FROM],
+    });
+    const res = validateBody(body);
+    expect(res).toStrictEqual({ ...TEST_TEMPLATE_RECORD, cc: [ENV_VARS.NODE_MAILER_FROM, ENV_VARS.NODE_MAILER_FROM] });
+  });
+
+  test('it should set the "bcc" field if a allowed recipient is provided', async () => {
+    const body = JSON.stringify({
+      ...TEST_TEMPLATE_RECORD,
+      bcc: [ENV_VARS.NODE_MAILER_FROM, ENV_VARS.NODE_MAILER_FROM],
+    });
+    const res = validateBody(body);
+    expect(res).toStrictEqual({ ...TEST_TEMPLATE_RECORD, bcc: [ENV_VARS.NODE_MAILER_FROM, ENV_VARS.NODE_MAILER_FROM] });
+  });
+
+  test('it should ignore the "from" field', async () => {
+    const body = JSON.stringify({ ...TEST_TEMPLATE_RECORD, from: 'foo@bar' });
+    const res = validateBody(body);
+    expect(res).toStrictEqual(TEST_TEMPLATE_RECORD);
+
+    const bodyArray = JSON.stringify({ ...TEST_TEMPLATE_RECORD, from: ['foo@bar', 'from@foo'] });
+    const resArray = validateBody(bodyArray);
+    expect(resArray).toStrictEqual(TEST_TEMPLATE_RECORD);
+  });
+});
 
 describe('mailer', { timeout: DEFAULT_TIMEOUT }, () => {
   test('it should filter the "to" field for unintended recipients and send an email to the default address', async () => {
